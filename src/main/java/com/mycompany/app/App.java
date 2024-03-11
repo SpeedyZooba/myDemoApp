a36fa4fb7e689caaffa0b09ac6d2d9a2f176d6eb
Görkem Revi <gorkemrevi@hotmail.com>
2024-03-11 21:40:00 +0300

    finalization of setup

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,30 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static boolean shiftComparison(String forwardString, ArrayList<Integer> forwardShifts, String backwardString, ArrayList<Integer> backwardShifts)
+    {
+        StringBuilder forwardBuilder = new StringBuilder();
+        StringBuilder backwardBuilder = new StringBuilder();
+        if (forwardString == null || backwardString == null || forwardShifts == null || backwardShifts == null)
+        {
+            throw new NullPointerException("Every input must be at least empty.");
+        }
+        for (int i = 0; i < forwardString.length(); i++)
+        {
+            forwardBuilder.append(forwardString.charAt(i) + forwardShifts.get(i));
+        }
+        for (int i = 0; i < backwardString.length(); i++)
+        {
+            backwardBuilder.append(backwardString.charAt(i) - backwardShifts.get(i));
+        }
+        return forwardBuilder.toString().equals(backwardBuilder.toString());
     }
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,23 +1,21 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public AppTest(String testName)
     {
-        super( testName );
+        super(testName);
     }
 
     /**
@@ -25,14 +23,102 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(AppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    public void testShiftEquals()
     {
-        assertTrue( true );
+        String fShiftString = "abc";
+        String bShiftString = "def";
+        ArrayList<Integer> fShiftIndexes = new ArrayList<Integer>();
+        ArrayList<Integer> bShiftIndexes = new ArrayList<Integer>();
+        for (int i = 0; i < fShiftString.length(); i++)
+        {
+            fShiftIndexes.add(1);
+        }
+        for (int i = 0; i < bShiftString.length(); i++)
+        {
+            bShiftIndexes.add(2);
+        }
+        assertTrue(App.shiftComparison(fShiftString, fShiftIndexes, bShiftString, bShiftIndexes));
+    }
+
+    public void testShiftNotEquals()
+    {
+        String fShiftString = "abc";
+        String bShiftString = "def";
+        ArrayList<Integer> fShiftIndexes = new ArrayList<Integer>();
+        ArrayList<Integer> bShiftIndexes = new ArrayList<Integer>();
+        for (int i = 0; i < 3; i++)
+        {
+            fShiftIndexes.add(1);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            bShiftIndexes.add(1);
+        }
+        assertFalse(App.shiftComparison(fShiftString, fShiftIndexes, bShiftString, bShiftIndexes));
+    }
+
+    public void testAtLeastOneInputIsNull()
+    {
+        try 
+        {
+            String bShiftString = "def";
+            ArrayList<Integer> fShiftIndexes = new ArrayList<Integer>();
+            ArrayList<Integer> bShiftIndexes = new ArrayList<Integer>();
+            for (int i = 0; i < 3; i++)
+            {
+                fShiftIndexes.add(1);
+            }
+            for (int i = 0; i < bShiftString.length(); i++)
+            {
+                bShiftIndexes.add(1);
+            }
+            boolean result = App.shiftComparison(null, fShiftIndexes, bShiftString, bShiftIndexes);
+            fail("Expected exception was not thrown, result being " + result);
+        }
+        catch (NullPointerException e)
+        {
+            assertEquals("Every input must be at least empty.", e.getMessage());
+        }
+    }
+
+    public void testIndexArrayIsEmpty()
+    {
+        try 
+        {
+            String fShiftString = "abc";
+            String bShiftString = "def";
+            ArrayList<Integer> fShiftIndexes = new ArrayList<Integer>();
+            ArrayList<Integer> bShiftIndexes = new ArrayList<Integer>();
+            for (int i = 0; i < bShiftString.length(); i++)
+            {
+                bShiftIndexes.add(1);
+            }
+            boolean result = App.shiftComparison(fShiftString, fShiftIndexes, bShiftString, bShiftIndexes);
+            fail("Expected exception was not thrown, result being " + result);
+        }
+        catch (IndexOutOfBoundsException e)
+        {
+            assertTrue(true);
+        }
+    }
+
+    public void testStringsAreEmpty()
+    {
+        String fShiftString = "";
+        String bShiftString = "";
+        ArrayList<Integer> fShiftIndexes = new ArrayList<Integer>();
+        ArrayList<Integer> bShiftIndexes = new ArrayList<Integer>();
+        for (int i = 0; i < 3; i++)
+        {
+            fShiftIndexes.add(1);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            bShiftIndexes.add(1);
+        }
+        assertTrue(App.shiftComparison(fShiftString, fShiftIndexes, bShiftString, bShiftIndexes));
     }
 }
